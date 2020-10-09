1985ba9e161665ecacf734c6f7226c0eea432814
Fooxboy <fooxboy@yandex.ru>
2020-10-09 18:36:06 +0300

    sdk был обновлнен к последней версии

--- Fooxboy.Boop.SDK/Api.cs
+++ Fooxboy.Boop.SDK/Api.cs
@@ -77,7 +77,7 @@ namespace Fooxboy.Boop.SDK
                     bytes = _stream.Read(data, 0, data.Length);
                 } while (_stream.DataAvailable);
 
-                var model = data.Deserialize<SocketRequest<object>>();
+                var model = (SocketRequest<object>) data.Deserialize();
                 
                 
                 //Много плохого кода простите, но мне лень, потом перепишу когда нибудь.
