0c79d2a2ab5009b66523f2ba244c86dc317c7559
Fooxboy <fooxboy@yandex.ru>
2020-10-30 23:14:28 +0300

    Добавлены диалоги и иконки

---
+++ Fooxboy.Boop.Client.WpfApp/ViewModels/DialogsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Fooxboy.Boop.Client.WpfApp.Services;
+
+namespace Fooxboy.Boop.Client.WpfApp.ViewModels
+{
+    public class DialogsViewModel:BaseViewModel
+    {
+        public async void GetDialogs()
+        {
+            var api = ApiService.Get();
+            var dialogs = await api.Messages.GetAsync(10);
+
+            
+        }
+    }
+}
