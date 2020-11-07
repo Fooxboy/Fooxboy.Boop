5ea8ec6742a494957d042d6497766c4990e9e1c2
Fooxboy <fooxboy@yandex.ru>
2020-11-07 03:12:30 +0300

    Добавлен контроллер в сервер

---
+++ Fooxboy.Boop.BackendServer/Controllers/Messages/CreateChat.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fooxboy.Boop.BackendServer.Controllers.Messages
+{
+    public class CreateChat : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
