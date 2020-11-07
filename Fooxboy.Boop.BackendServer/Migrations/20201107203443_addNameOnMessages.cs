3f1ae0affb85ec01b223d1188eafb06228b69c48
Fooxboy <fooxboy@yandex.ru>
2020-11-07 23:36:03 +0300

    Обновление базы данных

---
+++ Fooxboy.Boop.BackendServer/Migrations/20201107203443_addNameOnMessages.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fooxboy.Boop.BackendServer.Migrations
+{
+    public partial class addNameOnMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageSender",
+                table: "Messages",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameSender",
+                table: "Messages",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageSender",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "NameSender",
+                table: "Messages");
+        }
+    }
+}
