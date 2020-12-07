6f41cc9721113ed862d26685424ce1082aedefef
Fooxboy <Fooxboy@yandex.ru>
2020-12-07 03:40:28 +0300

    На сервер добавлена возможность получения списка друзей

---
+++ Fooxboy.Boop.Shared/Models/Friends/Get.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Fooxboy.Boop.Shared.Models.Users;
+
+namespace Fooxboy.Boop.Shared.Models.Friends
+{
+    public class Get
+    {
+        public List<User> Friends { get; set; }
+    }
+}
