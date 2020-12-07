8656581608eb3c8e75d8b4173b5d9406826fe8dd
Fooxboy <Fooxboy@yandex.ru>
2020-12-07 23:52:44 +0300

    На сервер добавлена возможность получения количества запросов в друзья

--- Fooxboy.Boop.BackendServer/Controllers/Friends/GetCountRequests.cs
+++ Fooxboy.Boop.BackendServer/Controllers/Friends/GetCountRequests.cs
@@ -11,18 +11,19 @@ namespace Fooxboy.Boop.BackendServer.Controllers.Friends
     [Produces("application/json")]
     [Route("api/friends.[controller]")]
     [ApiController]
-    public class GetRequested : Controller
+    public class GetCountRequests : Controller
     {
-        private Logger _logger;
+        public Logger _logger;
 
-        public GetRequested(Logger logger)
+        public GetCountRequests(Logger logger)
         {
             _logger = logger;
         }
+        
         [HttpGet]
         public Result Index(string token)
         {
-            _logger.Debug($"friends.getRequested?&token={token}");
+            _logger.Debug($"friends.getCountRequests?&token={token}");
             var result = new Result();
             
             if (Helpers.CheckerTokenHelper.GetUser(token) is null)
@@ -43,31 +44,15 @@ namespace Fooxboy.Boop.BackendServer.Controllers.Friends
                 var user = db.Users.SingleOrDefault(u => u.Token == token);
                 var friendsIds = new List<long>();
 
-                foreach (var idString in user.FriendsRequested.Split(","))
+                foreach (var idString in user.FirendsRequests.Split(","))
                 {
                     try
                     {
                         friendsIds.Add(long.Parse(idString));
                     }catch(InvalidCastException) {}
                 }
-                
-                var data = new Shared.Models.Friends.Get();
-                data.Friends = new List<Shared.Models.Users.User>();
-                foreach (var friendId in friendsIds)
-                {
-                    var friend = new Shared.Models.Users.User();
-                    var f = db.Users.Single(u => u.UserId == friendId);
-                    friend.Nickname = f.Nickname;
-                    friend.Number = f.Number;
-                    friend.FirstName = f.FirstName;
-                    friend.LastName = f.LastName;
-                    friend.UserId = f.UserId;
-                    friend.PathProfilePic = f.PathProfilePic;
-                    data.Friends.Add(friend);
-                }
-
                 result.Status = true;
-                result.Data = data;
+                result.Data = friendsIds.Count;
                 return result;
             }
         }
