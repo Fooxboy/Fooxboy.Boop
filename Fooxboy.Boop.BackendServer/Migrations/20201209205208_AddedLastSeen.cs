a2f01560f458fc9a0c2e79e321e6fb3086c2caf3
Fooxboy <Fooxboy@yandex.ru>
2020-12-09 23:56:34 +0300

    Послностью реализован LastSeen

---
+++ Fooxboy.Boop.BackendServer/Migrations/20201209205208_AddedLastSeen.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fooxboy.Boop.BackendServer.Migrations
+{
+    public partial class AddedLastSeen : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastSeenText",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastSeenText",
+                table: "Users");
+        }
+    }
+}
