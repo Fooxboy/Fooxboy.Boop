c44c0818de9bdb60fcfd9323e882f1d53f8ca3c1
Fooxboy <Fooxboy@yandex.ru>
2021-04-29 13:23:42 +0300

    Добавлены новые возможности

---
+++ Fooxboy.Boop.BackendServer/Migrations/20210429094316_addedaccess.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fooxboy.Boop.BackendServer.Migrations
+{
+    public partial class addedaccess : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AccessLevel",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EMail",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Group",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Specialty",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccessLevel",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EMail",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Group",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Specialty",
+                table: "Users");
+        }
+    }
+}
